4876a4707d94a76a7871d8b8d02670aea38b4627
caibate <caibate@gmail.com>
2016-10-02 14:48:25 -0300

    Configurando o flyway.

--- src/main/java/com/taks/api/TaskApiApplication.java
+++ src/main/java/com/taks/api/TaskApiApplication.java
@@ -17,30 +17,30 @@ public class TaskApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApiApplication.class, args);
 	}
-	@Bean
-	public CommandLineRunner demo(TaskRepository repository) {
-		return (args) -> {
-			//salva as Tasks
-			repository.save(new Task("Arrumar a casa", 50));
-			repository.save(new Task("Fazer comida", 100));
-			repository.save(new Task("Estudar", 70));
-			
-			// fetch all customers
-			log.info("Tasks found with findAll():");
-			log.info("-------------------------------");
-			for (Task task: repository.findAll()) {
-				log.info(task.toString());
-			}
-            log.info("");
-
-			// fetch an individual customer by ID
-			Task task1 = repository.findOne(1);
-			log.info("Task1 found with findOne(1L):");
-			log.info("--------------------------------");
-			log.info(task1.toString());
-            log.info("");
-
-
-		};
-	}
+//	@Bean
+//	public CommandLineRunner demo(TaskRepository repository) {
+//		return (args) -> {
+//			//salva as Tasks
+//			repository.save(new Task("Arrumar a casa", 50));
+//			repository.save(new Task("Fazer comida", 100));
+//			repository.save(new Task("Estudar", 70));
+//			
+//			// fetch all customers
+//			log.info("Tasks found with findAll():");
+//			log.info("-------------------------------");
+//			for (Task task: repository.findAll()) {
+//				log.info(task.toString());
+//			}
+//            log.info("");
+//
+//			// fetch an individual customer by ID
+//			Task task1 = repository.findOne(1);
+//			log.info("Task1 found with findOne(1L):");
+//			log.info("--------------------------------");
+//			log.info(task1.toString());
+//            log.info("");
+//
+//
+//		};
+//	}
 }
